d6cab24f034b8f1dcab17121f97f88f974eb6e1f
exa02062 <rsemolaleal@claro.com.ar>
2023-09-10 01:42:45 -0300

    Primera entrega

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -2,8 +2,7 @@
 const Brand = () => {
     return (
         <div className="navbar-brand bg-dark d-flex align-items-center">
-            <img src="favicon.ico" className="rounded" alt="ReCuerda" width="30" height="30">
-            </img>
+            <img src="favicon.ico" className="rounded" alt="ReCuerda" width="30" height="30"/>
             <span className="navbar-brand p-2">ReCuerda</span>
         </div>
     )
